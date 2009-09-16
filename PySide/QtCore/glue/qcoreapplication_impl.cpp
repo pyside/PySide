c58db32367e278504bf787d149364f86fd2204fa
Hugo Lima <hugo.lima@openbossa.org>
2009-09-16 17:44:44 -0300

    Added inject code for QCoreApplication constructor.

---
+++ PySide/QtCore/glue/qcoreapplication_impl.cpp
@@ -0,0 +1,26 @@
+PyObject* PyQCoreApplication_New(PyTypeObject *type, PyObject *args, PyObject*)
+{
+    int numArgs = PyTuple_GET_SIZE(args);
+    if (numArgs != 1) {
+        PyErr_BadArgument();
+        return 0;
+    }
+
+
+    char** argv;
+    int argc;
+    if (!PySequence_to_argc_argv(PyTuple_GET_ITEM(args, 0), &argc, &argv)) {
+        PyErr_BadArgument();
+        return 0;
+    }
+
+    QCoreApplication* cptr = new QCoreApplication(argc, argv);
+    PyObject* self = Shiboken::PyBaseWrapper_New(type, &PyQCoreApplication_Type, cptr);
+
+    if (!self) {
+        if (cptr) delete cptr;
+        PyErr_BadInternalCall();
+        return 0;
+    }
+    return self;
+}
