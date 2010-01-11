cb7c8ad32befcadb35d02c201902dcc6b6232e18
Hugo Lima <hugo.lima@openbossa.org>
2010-01-11 19:21:48 -0200

    Adapt some injected codes to the recent changes related to object initialization. (tp_init/tp_new).

--- PySide/QtCore/glue/qcoreapplication_impl.cpp
+++ PySide/QtCore/glue/qcoreapplication_impl.cpp
@@ -1,25 +1,20 @@
-PyObject* SbkQCoreApplication_New(Shiboken::SbkBaseWrapperType* type, PyObject* args, PyObject*)
+int SbkQCoreApplication_Init(PyObject* self, PyObject* args, PyObject*)
 {
     int numArgs = PyTuple_GET_SIZE(args);
     if (numArgs != 1) {
         PyErr_BadArgument();
-        return 0;
+        return -1;
     }
 
     char** argv;
     int argc;
     if (!PySequence_to_argc_argv(PyTuple_GET_ITEM(args, 0), &argc, &argv)) {
         PyErr_BadArgument();
-        return 0;
+        return -1;
     }
 
-    QCoreApplication* cptr = new QCoreApplication(argc, argv);
-    PyObject* self = Shiboken::SbkBaseWrapper_New(type, cptr);
-
-    if (!self) {
-        if (cptr) delete cptr;
-        PyErr_BadInternalCall();
-        return 0;
-    }
-    return self;
+    SbkBaseWrapper_setCptr(self, new QCoreApplication(argc, argv));
+    SbkBaseWrapper_setValidCppObject(self, 1);
+    Shiboken::BindingManager::instance().registerWrapper(reinterpret_cast<SbkBaseWrapper*>(self));
+    return 1;
 }
