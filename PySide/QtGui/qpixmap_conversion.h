e864bdf57e2c549ad5f8f6f8b966dadca6f1ee68
Renato Filho <renato.filho@openbossa.org>
2010-02-01 15:29:09 -0300

    QPixmap conversion from QVariant.
    Reviewed by Hugo Parente <hugo.lima@openbossa.org>

---
+++ PySide/QtGui/qpixmap_conversion.h
@@ -0,0 +1,27 @@
+inline bool Converter< QPixmap >::isConvertible(PyObject* pyobj)
+{
+    bool isVariant = SbkQVariant_Check(pyobj);
+    if (isVariant) {
+        QVariant var(Converter<QVariant>::toCpp(pyobj));
+        return var.type() == QVariant::Pixmap;
+    } else if (SbkQSize_Check(pyobj) || SbkQString_Check(pyobj)) {
+        return true;
+    }
+    return false;
+
+}
+
+inline QPixmap Converter< QPixmap >::toCpp(PyObject* pyobj)
+{
+    bool isVariant = SbkQVariant_Check(pyobj);
+    if (isVariant) {
+        QVariant var(Converter<QVariant>::toCpp(pyobj));
+        return var.value<QPixmap>();
+    } else if (SbkQSize_Check(pyobj)) {
+        return QPixmap(Shiboken::Converter<QSize& >::toCpp(pyobj));
+    } else if (SbkQString_Check(pyobj)) {
+        return QPixmap(Shiboken::Converter<QString& >::toCpp(pyobj));
+    }
+
+    return *SbkQPixmap_cptr(pyobj);
+}
