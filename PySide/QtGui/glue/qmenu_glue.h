2cee6b6fca0c9b4188b7fb10643f77a719b792fc
Renato Filho <renato.filho@openbossa.org>
2010-02-01 20:34:18 -0300

    Ported boost::python inject code for QMenu and QShortcut.
    Reviewed by Hugo Parente Lima <hugo.lima@openbossa.org>

---
+++ PySide/QtGui/glue/qmenu_glue.h
@@ -0,0 +1,23 @@
+
+inline PyObject*
+addActionWithPyObject(QMenu *self, const QIcon& icon, const QString& text, PyObject *callback, const QKeySequence &shortcut)
+{
+    QAction *act = new QAction(text, self);
+
+    if (!icon.isNull())
+        act->setIcon(icon);
+
+    if (!shortcut.isEmpty())
+        act->setShortcut(shortcut);
+
+    self->addAction(act);
+
+    PyObject *pyAct = Shiboken::Converter<QAction*>::toPython(act);
+    PyObject* result = PyObject_CallMethod(pyAct, "connect", "OsO", pyAct, SIGNAL(triggered(bool)), callback);
+    if (result == 0) {
+        Py_DECREF(pyAct);
+        return 0;
+    }
+
+    return pyAct;
+}
