8e8c65f888707143e19b124e9b82b25ffc27a9da
Lauro Neto <lauro.neto@openbossa.org>
2010-02-11 16:51:50 -0300

    Adding qabs_test.py
    Reviewed by Marcelo Lira <marcelo.lira@openbossa.org>

---
+++ tests/qtcore/qabs_test.py
@@ -0,0 +1,20 @@
+
+import unittest
+
+from PySide import QtCore
+
+
+class QAbsTest(unittest.TestCase):
+    '''Tests for QtCore.qAbs'''
+
+    def testBasic(self):
+        def check(x):
+            self.assertEqual(QtCore.qAbs(x), abs(x))
+
+        check(0)
+        check(-10)
+        check(10)
+
+
+if __name__ == '__main__':
+    unittest.main()
