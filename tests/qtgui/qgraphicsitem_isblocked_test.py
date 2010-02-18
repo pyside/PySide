9f47f2174215eb49f3060669db66c7145e2ee7e5
Bruno dos Santos de Araujo <bruno.araujo@openbossa.org>
2010-02-18 11:36:11 -0400

    Add unit test for QGraphicsItem.isBlockedByModalPanel()

---
+++ tests/qtgui/qgraphicsitem_isblocked_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import unittest
+
+from PySide import QtGui, QtCore
+from helper import UsesQApplication
+
+class Item(QtGui.QGraphicsItem):
+    
+    def __init__(self):
+        QtGui.QGraphicsItem.__init__(self)
+
+    def boundingRect(self):
+        return QtCore.QRectF(0, 0, 100, 100)
+    
+    def paint(self, painter, option, widget):
+        painter.setBrush(QtGui.QColor(255, 255, 255))
+        painter.drawRect(0, 0, 100, 100)
+
+
+class QGraphicsViewIsBlockedTest(UsesQApplication):
+
+    def testIsBlockedByModalPanel(self):
+        item = Item()
+        item.isBlockedByModalPanel()
+
+if __name__ == "__main__":
+    unittest.main()
