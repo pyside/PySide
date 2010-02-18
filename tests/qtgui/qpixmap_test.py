aa2a2a57030dec2e8b73b017de5f157aae0fb5e5
Luciano Wolf <luciano.wolf@openbossa.org>
2010-02-18 16:33:23 -0300

    Improve qpixmap test to support qstring and qsize arguments.
    Reviewed by Marcelo Lira <marcelo.lira@openbossa.org>

--- tests/qtgui/qpixmap_test.py
+++ tests/qtgui/qpixmap_test.py
@@ -2,14 +2,20 @@ import unittest
 
 from helper import UsesQApplication
 from PySide.QtGui import QPixmap
-from PySide.QtCore import QVariant
+from PySide.QtCore import QVariant, QSize, QString
 
-#Only test if is possible create a QPixmap from a QVariant
 class QPixmapTest(UsesQApplication):
     def testQVariantConstructor(self):
         pixmap = QPixmap()
         v = QVariant(pixmap)
         pixmap_copy = QPixmap(v)
+
+    def testQSizeConstructor(self):
+        pixmap = QPixmap(QSize(10,20))
+        self.assert_(pixmap.size().height(), 20)
+
+    def testQStringConstructor(self):
+        pixmap = QPixmap(QString("Testing!"))
 
 if __name__ == '__main__':
     unittest.main()
