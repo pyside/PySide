18302e80b874f3032e17abe1deab03b92232711a
Marcelo Lira <marcelo.lira@openbossa.org>
2010-02-25 13:29:54 -0300

    Renamed QStyle test to StandardPixmap.

--- tests/qtgui/standardpixmap_test.py
+++ tests/qtgui/standardpixmap_test.py
@@ -5,10 +5,11 @@ from PySide.QtGui import QPixmap, QStyle
 
 from helper import UsesQApplication
 
-class StandardPixmap(UsesQApplication):
+class StandardPixmapTest(UsesQApplication):
     def testDefaultOptions(self): # Bug 253
         pixmap = self.app.style().standardPixmap(QStyle.SP_DirClosedIcon)
         self.assert_(isinstance(pixmap, QPixmap))
 
 if __name__ == '__main__':
     unittest.main()
+
