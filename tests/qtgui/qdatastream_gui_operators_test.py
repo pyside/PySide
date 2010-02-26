2c73d919273b1660af40053afa198a42278c07a9
Lauro Neto <lauro.neto@openbossa.org>
2010-02-26 20:16:54 -0300

    Fix QDataStream <</>> QPixmap test

--- tests/qtgui/qdatastream_gui_operators_test.py
+++ tests/qtgui/qdatastream_gui_operators_test.py
@@ -3,17 +3,33 @@
 import unittest
 import sys
 
-from PySide import QtGui, QtCore
+from PySide.QtCore import QDataStream, QByteArray, QIODevice, Qt
+from PySide.QtGui import QPixmap, QColor
 
+from helper import UsesQApplication
 
-class QAppPresence(unittest.TestCase):
+class QPixmapQDatastream(UsesQApplication):
+    '''QDataStream <<>> QPixmap'''
 
-    def testQPixmap(self):
-        ds = QtCore.QDataStream()
-        p = QtGui.QPixmap()
-        ds << p
-        ds >> p
+    def setUp(self):
+        super(QPixmapQDatastream, self).setUp()
+        self.source_pixmap = QPixmap(100, 100)
+        self.source_pixmap.fill(Qt.red)
+        self.output_pixmap = QPixmap()
+        self.buffer = QByteArray()
+        self.read_stream = QDataStream(self.buffer, QIODevice.ReadOnly)
+        self.write_stream = QDataStream(self.buffer, QIODevice.WriteOnly)
+
+    def testStream(self):
+        self.write_stream << self.source_pixmap
+
+        self.read_stream >> self.output_pixmap
+
+        image = self.output_pixmap.toImage()
+        pixel = image.pixel(10,10)
+        self.assertEqual(pixel, QColor(Qt.red).rgba())
+        self.assertEqual(self.source_pixmap.toImage(), self.output_pixmap.toImage())
+
 
 if __name__ == '__main__':
-    app = QtGui.QApplication([])
     unittest.main()
