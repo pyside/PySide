ceae06a748c5120eee89a2983b1cc994c09844ca
Marcelo Lira <marcelo.lira@openbossa.org>
2010-03-04 18:38:56 -0300

    Extends the QKeySequence unit test.
    The test adds conversions from QString and Python string to a
    QKeySequence.
    
    Reviewed by Hugo Parente <hugo.lima@openbossa.org>

--- tests/qtgui/qstring_qkeysequence_test.py
+++ tests/qtgui/qstring_qkeysequence_test.py
@@ -1,17 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-'''Tests QString using QKeySequence parameter'''
+'''Tests conversions of QString to and from QKeySequence.'''
 
 import unittest
+from helper import UsesQApplication
 
 from PySide.QtCore import QString
-from PySide.QtGui import QKeySequence
+from PySide.QtGui import QKeySequence, QAction
 
-class QStringQKeySequenceTest(unittest.TestCase):
-    '''Tests QString using QKeySequence parameter'''
-    
-    def testQStringQKeySequence(self):
-        a = QString(QKeySequence("Ctrl+A"))
+class QStringQKeySequenceTest(UsesQApplication):
+    '''Tests conversions of QString to and from QKeySequence.'''
+
+    def testQStringFromQKeySequence(self):
+        '''Creates a QString from a QKeySequence.'''
+        keyseq = 'Ctrl+A'
+        a = QString(QKeySequence(keyseq))
+        self.assertEqual(a, keyseq)
+
+    def testQStringAsQKeySequence(self):
+        '''Passes a QString to an argument expecting a QKeySequence.'''
+        keyseq = QString('Ctrl+A')
+        action = QAction(None)
+        action.setShortcut(keyseq)
+        shortcut = action.shortcut()
+        self.assert_(isinstance(shortcut, QKeySequence))
+        self.assertEqual(shortcut.toString(), keyseq)
+
+    def testPythonStringAsQKeySequence(self):
+        '''Passes a Python string to an argument expecting a QKeySequence.'''
+        keyseq = 'Ctrl+A'
+        action = QAction(None)
+        action.setShortcut(keyseq)
+        shortcut = action.shortcut()
+        self.assert_(isinstance(shortcut, QKeySequence))
+        self.assertEqual(shortcut.toString(), keyseq)
 
 if __name__ == '__main__':
     unittest.main()
