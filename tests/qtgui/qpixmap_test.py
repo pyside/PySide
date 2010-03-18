c4896b83723adc7050d486450b9758e5211dacb5
Lauro Neto <lauro.neto@openbossa.org>
2010-03-18 16:37:31 -0300

    Adding a more complex test for QPixmap, QColor, etc

--- tests/qtgui/qpixmap_test.py
+++ tests/qtgui/qpixmap_test.py
@@ -37,6 +37,26 @@ class QPixmapTest(UsesQApplication):
         pixmap = QPixmap()
         self.assert_(pixmap.loadFromData(data))
 
+
+class QPixmapToImage(UsesQApplication):
+
+    def testFilledImage(self):
+        '''QPixmap.fill + toImage + image.pixel'''
+        pixmap = QPixmap(100, 200)
+        pixmap.fill(Qt.red) # Default Qt.white
+
+        self.assertEqual(pixmap.height(), 200)
+        self.assertEqual(pixmap.width(), 100)
+
+        image = pixmap.toImage()
+
+        self.assertEqual(image.height(), 200)
+        self.assertEqual(image.width(), 100)
+
+        pixel = image.pixel(10,10)
+        self.assertEqual(pixel, QColor(Qt.red).rgba())
+
+
 if __name__ == '__main__':
     unittest.main()
 
