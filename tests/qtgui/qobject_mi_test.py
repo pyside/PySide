965a16322bade4098b8fe187296e62b746d6b590
Lauro Neto <lauro.neto@openbossa.org>
2010-04-09 17:49:31 -0300

    Adding test for multiple QObject inheritance
    Reviewer: Hugo Lima <hugo.lima@openbossa.org>
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/qtgui/qobject_mi_test.py
@@ -0,0 +1,37 @@
+﻿'''Test cases for multiple inheritance from 2 QObjects'''
+
+import unittest
+
+from PySide.QtCore import QObject
+from PySide.QtGui import *
+
+from helper import UsesQApplication
+
+class WidgetValidator(QWidget, QIntValidator):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        QIntValidator.__init__(self, parent)
+
+
+class DoubleQObjectInheritanceTest(UsesQApplication):
+
+    def testDouble(self):
+        '''Double inheritance from QObject classes'''
+
+        obj = WidgetValidator()
+
+        #QObject methods
+        obj.setObjectName('aaaa')
+        self.assertEqual(obj.objectName(), 'aaaa')
+
+        #QWidget methods
+        obj.setVisible(False)
+        self.assertFalse(obj.isVisible())
+
+        #QIntValidator methods
+        self.assertEqual(obj.validate('aaaa', 0), QValidator.Invalid)
+        self.assertEqual(obj.validate('33', 0), QValidator.Acceptable)
+
+
+if __name__ == '__main__':
+    unittest.main()
