0255f6ebb65850e593d34da4804d2bad2ad486bc
Luciano Wolf <luciano.wolf@openbossa.org>
2010-04-21 13:07:45 -0300

    Adding support to QtScript module.

--- PySide/__init__.py
+++ PySide/__init__.py
@@ -1,1 +1,1 @@
-__all__ = ['QtCore', 'QtGui', 'QtNetwork', 'QtOpenGL', 'QtSql', 'QtSvg', 'QtTest', 'QtWebKit']
+__all__ = ['QtCore', 'QtGui', 'QtNetwork', 'QtOpenGL', 'QtSql', 'QtSvg', 'QtTest', 'QtWebKit', 'QtScript']
