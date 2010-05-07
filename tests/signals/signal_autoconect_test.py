f17539378b995711c011b74070509566f30c7e9a
Renato Filho <renato.filho@openbossa.org>
2010-05-07 11:47:43 -0300

    unittest for autoconection signal/slot.
    Reviewer: Hugo Parente Lima <hugo.lima@openbossa.org>,
              Luciano Wolf <luciano.wolf@openbossa.org

---
+++ tests/signals/signal_autoconect_test.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+class MyObject(QWidget):
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        self._method_called = False
+
+    def on_button_clicked(self):
+        self._method_called = True
+
+
+class AutoConnectionTest(unittest.TestCase):
+
+    def testConnection(self):
+        app = QApplication([])
+
+        win = MyObject()
+        btn = QPushButton("click", win)
+        btn.setObjectName("button")
+        QMetaObject.connectSlotsByName(win)
+        btn.click()
+        self.assert_(win._method_called)
+
+if __name__ == '__main__':
+    unittest.main()
