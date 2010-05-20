2956c15cb2aec0d0450a4c3272af82a8a2e37450
Renato Filho <renato.filho@openbossa.org>
2010-05-20 14:41:23 -0300

    Append license and copyright to pyhtml2devhelp.py script
    Reviewer: Hugo Parente Lima <hugo.lima@openbossa.org>,
              Luciano Wolf <luciano.wolf@openbossa.org>

--- doc/pyhtml2devhelp.py
+++ doc/pyhtml2devhelp.py
@@ -1,6 +1,35 @@
-#! /usr/bin/python
+"""
+  This script is based on Python2.6 Ubuntu package script:
+  Copyright (C) 2009  Matthias Klose <doko@debian.org>
+  Copyright (C) 2010  Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
 
-# This script is based on Python2.6 Ubuntu package script by Marc Deslauriers
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public License
+  version 2.1 as published by the Free Software Foundation. Please
+  review the following information to ensure the GNU Lesser General
+  Public License version 2.1 requirements will be met:
+  http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+
+  As a special exception to the GNU Lesser General Public License
+  version 2.1, the object code form of a "work that uses the Library"
+  may incorporate material from a header file that is part of the
+  Library.  You may distribute such object code under terms of your
+  choice, provided that the incorporated material (i) does not exceed
+  more than 5% of the total size of the Library; and (ii) is limited to
+  numerical parameters, data structure layouts, accessors, macros,
+  inline functions and templates.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+   02110-1301 USA
+"""
+#! /usr/bin/python
 
 import formatter, htmllib
 import os, sys, re
@@ -53,7 +82,7 @@ class PyHTMLParser(htmllib.HTMLParser):
         for attr in attrs:
             self.link[attr[0]] = attr[1]
         self.data = ''
-        
+
     def end_a(self):
         process = False
         text = self.data.replace('\t', '').replace('\n', ' ').replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
@@ -185,7 +214,7 @@ class PyIdxHTMLParser(htmllib.HTMLParser):
         for attr in attrs:
             self.link[attr[0]] = attr[1]
         self.data = ''
-        
+
     def end_a(self):
         text = self.data.replace('\t', '').replace('\n', ' ').replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
         self.link['text'] = text
