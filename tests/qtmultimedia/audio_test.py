92a07a7a15394b3f0a01004988d6025e7c8f1e9e
Renato Filho <renato.filho@openbossa.org>
2010-05-24 15:47:57 -0300

    QtMultimedia bindings.
    Reviewer: Hugo Parente Lima <hugo.lima@openbossa.org>,
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/qtmultimedia/audio_test.py
@@ -0,0 +1,32 @@
+
+'''Test cases for QHttp'''
+
+import unittest
+
+from PySide.QtCore import *
+from PySide.QtMultimedia import *
+
+class testAudioDevices(unittest.TestCase):
+
+    def testListDevices(self):
+        for devInfo in QAudioDeviceInfo.availableDevices(QAudio.AudioOutput):
+            fmt = QAudioFormat()
+            for codec in devInfo.supportedCodecs():
+                fmt.setCodec(codec)
+                for frequency in devInfo.supportedFrequencies():
+                    fmt.setFrequency(frequency)
+                    for channels in devInfo.supportedChannels():
+                        fmt.setChannels(channels)
+                        for sampleType in devInfo.supportedSampleTypes():
+                            fmt.setSampleType(sampleType)
+                            for sampleSize in devInfo.supportedSampleSizes():
+                                fmt.setSampleSize(sampleSize)
+                                for endian in devInfo.supportedByteOrders():
+                                    fmt.setByteOrder(endian)
+                                    if devInfo.isFormatSupported(fmt):
+                                        return
+        self.assert_(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
