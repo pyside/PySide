4090ef81efcada5bd6f882e1c91cd3a699c9c034
Renato Filho <renato.filho@openbossa.org>
2010-07-09 10:31:37 -0300

    Restore missing function on QStandardItemModel.
    Created unit test.
    
    Fixes bug: #257.
    
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

--- tests/QtGui/qstandarditemmodel_test.py
+++ tests/QtGui/qstandarditemmodel_test.py
@@ -1,7 +1,7 @@
 import unittest
+import sys
 
-from PySide.QtGui import *
-from PySide.QtCore import *
+from PySide.QtGui import QStandardItemModel, QWidget, QStandardItem
 
 from helper import UsesQApplication
 
@@ -20,6 +20,43 @@ class QStandardItemModelTest(UsesQApplication):
     def testInsertRow(self):
         # bug #227
         self.model.insertRow(0)
+
+
+class QStandardItemModelRef(UsesQApplication):
+    def testRefCount(self):
+        model = QStandardItemModel(5, 5)
+        items = []
+        for r in range(5):
+            row = []
+            for c in range(5):
+                row.append(QStandardItem("%d,%d" % (r,c)) )
+                self.assertEqual(sys.getrefcount(row[c]), 2)
+
+            model.insertRow(r, row)
+
+            for c in range(5):
+                ref_after = sys.getrefcount(row[c])
+                # check if the ref count was incremented after insertRow
+                self.assertEqual(ref_after, 3)
+
+            items.append(row)
+            row = None
+
+        for r in range(3):
+            my_row = model.takeRow(0)
+            my_row = None
+            for c in range(5):
+                # only rest 1 reference
+                self.assertEqual(sys.getrefcount(items[r][c]), 2)
+
+        my_i = model.item(0,0)
+        # ref(my_i) + parent_ref + items list ref
+        self.assertEqual(sys.getrefcount(my_i), 4)
+
+        model.clear()
+        # ref(my_i)
+        self.assertEqual(sys.getrefcount(my_i), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
