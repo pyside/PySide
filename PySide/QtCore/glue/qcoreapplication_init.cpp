505ba23a9d63e7c6d014f3c5bd5b1cdf778aac56
Marcelo Lira <marcelo.lira@openbossa.org>
2010-07-27 10:01:33 -0300

    Fixed glue code for QApplication and QCoreApplication.
    The code for QApplication deletion should also invalidate the
    wrapper for it. Also the similar code for QCoreApplication must
    perform the same steps as QApplication.
    
    Reviewed by Luciano Wolf <luciano.wolf@openbossa.org>
    Reviewed by Renato Araújo <renato.filho@openbossa.org>

--- PySide/QtCore/glue/qcoreapplication_init.cpp
+++ PySide/QtCore/glue/qcoreapplication_init.cpp
@@ -7,7 +7,9 @@ static char** QCoreApplicationArgValues;
  */
 void DeleteQCoreApplicationAtExit() {
     if (QCoreApplication::instance()) {
-        delete QCoreApplication::instance();
+        PySide::SignalManager::instance().clear();
+        BindingManager::instance().invalidateWrapper(QCoreApplication::instance());
+        QCoreApplication::instance()->deleteLater();
         for (int i = 0; i < QCoreApplicationArgCount; ++i)
             delete[] QCoreApplicationArgValues[i];
     }
