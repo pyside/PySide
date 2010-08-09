a3b0314c58720194613a87623461abc93469a978
Lauro Neto <lauro.neto@openbossa.org>
2010-08-09 18:03:24 -0300

    Adding deepcopy test for QtCore classes.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Renato Filho <renato.filho@openbossa.org>

---
+++ tests/QtCore/deepcopy_test.py
@@ -0,0 +1,56 @@
+
+import unittest
+from copy import deepcopy
+
+from PySide.QtCore import QByteArray, QDate, QDateTime, QTime
+from PySide.QtCore import Qt, QSize, QSizeF, QRect, QRectF
+
+class DeepCopyHelper:
+    
+    def testCopy(self):
+        copy = deepcopy([self.original])[0]
+        self.assert_(copy is not self.original)
+        self.assertEqual(copy, self.original)
+
+class QByteArrayDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QByteArray('the quick brown fox jumps over the lazy dog')
+
+
+class QDateDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QDate(2010, 11, 22)
+
+
+class QTimeDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QTime(11, 37, 55, 692)
+
+
+class QDateTimeDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QDateTime(2010, 5, 18, 10, 24, 45, 223, Qt.LocalTime)
+
+
+class QSizeDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QSize(42, 190)
+
+
+class QSizeFDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QSizeF(42.7, 190.2)
+
+
+class QRectDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QRect(100, 200, 300, 400)
+
+
+class QRectFDeepCopy(DeepCopyHelper, unittest.TestCase):
+    def setUp(self):
+        self.original = QRectF(100.33, 200.254, 300.321, 400.123)
+
+
+if __name__ == '__main__':
+    unittest.main()
