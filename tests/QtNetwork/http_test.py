1162a844bd689958205d10eeff549451ee8cb93b
Renato Filho <renato.filho@openbossa.org>
2010-08-13 17:25:57 -0300

    Fixed Http test to work with Qt 4.7.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

--- tests/QtNetwork/http_test.py
+++ tests/QtNetwork/http_test.py
@@ -6,18 +6,17 @@ import unittest
 from PySide.QtCore import *
 from PySide.QtNetwork import *
 
-from helper import UsesQApplication
+from helper import UsesQCoreApplication
 from httpd import TestServer
 
-class HttpSignalsCase(UsesQApplication):
+class HttpSignalsCase(UsesQCoreApplication):
     '''Test case for launching QHttp signals'''
 
     def setUp(self):
         super(HttpSignalsCase, self).setUp()
-        self.http = QHttp()
         self.httpd = TestServer()
         self.httpd.start()
-        self.url = QUrl('localhost:' + str(self.httpd.port()))
+        self.http = QHttp('localhost' , self.httpd.port())
         self.called = False
 
     def tearDown(self):
@@ -34,8 +33,7 @@ class HttpSignalsCase(UsesQApplication):
         #QHttp signal requestStarted signal
         # @bug 114
         self.http.requestStarted.connect(self.callback)
-        self.http.get(self.url.path())
-
+        self.http.get("index.html")
         self.app.exec_()
         self.assert_(self.called)
 
