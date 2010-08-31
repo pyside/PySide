b9bf521a93785ccd935cc4ce1464cb612c4fdbfe
Renato Filho <renato.filho@openbossa.org>
2010-08-31 10:59:04 -0300

    Unittest to bug #324.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

--- tests/QtGui/qpushbutton_test.py
+++ tests/QtGui/qpushbutton_test.py
@@ -25,6 +25,17 @@ class QPushButtonTest(UsesQApplication):
         timer = QTimer.singleShot(100, self.app.quit)
         self.app.exec_()
 
+    def buttonCb(self, checked):
+        self._clicked = True
+
+    def testBoolinSignal(self):
+        b = QPushButton()
+        b.setCheckable(True)
+        self._clicked = False
+        b.toggled[bool].connect(self.buttonCb)
+        b.toggle()
+        self.assert_(self._clicked)
+
 if __name__ == '__main__':
     unittest.main()
 
