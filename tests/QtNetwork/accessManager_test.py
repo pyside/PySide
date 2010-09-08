e5348268a63a8fd15519c703c4e6e7feb34f4282
Hugo Parente Lima <hugo.pl@gmail.com>
2010-09-08 15:31:41 -0300

    Make accessManager test do not depend on internet access.

--- tests/QtNetwork/accessManager_test.py
+++ tests/QtNetwork/accessManager_test.py
@@ -6,18 +6,33 @@ import unittest
 from PySide.QtCore import *
 from PySide.QtNetwork import *
 
-from helper import UsesQApplication
+from helper import UsesQCoreApplication
+from httpd import TestServer
 
-class AccessManagerCase(UsesQApplication):
+class AccessManagerCase(UsesQCoreApplication):
+
+    def setUp(self):
+        super(AccessManagerCase, self).setUp()
+        self.httpd = TestServer()
+        self.httpd.start()
+        self.called = False
+
+    def tearDown(self):
+        super(AccessManagerCase, self).tearDown()
+        self.httpd.shutdown()
+
     def slot_replyFinished(self, reply):
         self.assertEqual(type(reply), QNetworkReply)
+        self.called = True
         self.app.quit()
 
     def testNetworkRequest(self):
         manager = QNetworkAccessManager()
         manager.finished.connect(self.slot_replyFinished)
-        manager.get(QNetworkRequest(QUrl("http://qt.nokia.com")))
+        manager.get(QNetworkRequest(QUrl("http://localhost:%s" % self.httpd.port())))
         self.app.exec_()
+        self.assert_(self.called)
+        self.httpd.shutdown()
 
 if __name__ == '__main__':
     unittest.main()
