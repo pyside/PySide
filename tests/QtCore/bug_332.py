2559ae50f05f5135f88361fdc60f558c382d0b80
renatofilho <renato.filho@openbossa.org>
2010-09-14 18:19:15 -0300

    Created unittest for bug #332.
    Reviewer: Hugo Parente Lima <hugo.pl@gmail.com>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/QtCore/bug_332.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import unittest
+from PySide import QtCore
+
+class Lock(QtCore.QMutex):
+    def tryLock(self,timeoutt=10):
+        return QtCore.QMutex.tryLock(self,timeoutt)
+
+class TestBug(unittest.TestCase):
+
+    def testCase(self):
+        l = Lock()
+        l.tryLock() # this cause a assertion
+
+
