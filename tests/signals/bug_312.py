1217b7df39a9040d0c579727fa9d15ebb78cff4a
renatofilho <renato.filho@openbossa.org>
2010-09-16 14:15:12 -0300

    Update unit test for bug 312.
    Now the test verify if is possible connect more then 500 signals, if the signals was disconnected.
    
    Reviewer: Hugo Parente Lima <hugo.pl@gmail.com>
              Luciano Wolf <luciano.wolf@openbossa.org>

--- tests/signals/bug_312.py
+++ tests/signals/bug_312.py
@@ -2,7 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 from PySide.QtCore import QObject, SIGNAL
+
+class Dummy(object):
+    def __init__(self, parent):
+        self._parent = parent
+
+    def callback(self):
+        self._called = True
 
 class MultipleSlots(unittest.TestCase):
     def myCB(self):
@@ -16,6 +24,21 @@ class MultipleSlots(unittest.TestCase):
 
         o.emit(SIGNAL("fire()"))
         self.assertEqual(self._count, 200)
+
+    def testDisconnectCleanup(self):
+        for c in range(5):
+            self._count = 0
+            self._senders = []
+            for i in range(200):
+                o = QObject()
+                QObject.connect(o, SIGNAL("fire()"), lambda: self.myCB())
+                self._senders.append(o)
+                o.emit(SIGNAL("fire()"))
+
+            self.assertEqual(self._count, 200)
+
+            #delete all senders will disconnect the signals
+            self._senders = []
 
 if __name__ == '__main__':
     unittest.main()
