3525b77d69937938aa285f84b995a3a2d8685cce
renatofilho <renato.filho@openbossa.org>
2010-09-22 16:59:04 -0300

    Created unit test for bug #376.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Marcelo Lira <marcelo.lira@openbossa.org>

---
+++ tests/QtUiTools/bug_376.py
@@ -0,0 +1,19 @@
+import unittest
+import os
+from helper import UsesQApplication
+
+from PySide import QtCore, QtGui
+from PySide.QtUiTools import QUiLoader
+
+class BugTest(UsesQApplication):
+    def testCase(self):
+        w = QtGui.QWidget()
+        loader = QUiLoader()
+
+        filePath = os.path.join(os.path.dirname(__file__), 'test.ui')
+        result = loader.load(filePath, w)
+        self.assertEqual(type(result.child_object), QtGui.QFrame)
+
+if __name__ == '__main__':
+    unittest.main()
+
