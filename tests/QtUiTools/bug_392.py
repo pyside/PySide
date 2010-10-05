4adb686fc15dc3dfdb872157df27b534f1ca7f98
renatofilho <renato.filho@openbossa.org>
2010-10-05 12:07:08 -0300

    Extend QUiLoader test to test  ui files with custom widgets.

--- tests/QtUiTools/bug_392.py
+++ tests/QtUiTools/bug_392.py
@@ -2,7 +2,7 @@ import unittest
 import os
 from helper import UsesQApplication
 
-from PySide import QtCore, QtGui
+from PySide import QtCore, QtGui, QtDeclarative
 from PySide.QtUiTools import QUiLoader
 
 class BugTest(UsesQApplication):
@@ -13,6 +13,16 @@ class BugTest(UsesQApplication):
         filePath = os.path.join(os.path.dirname(__file__), 'action.ui')
         result = loader.load(filePath, w)
         self.assertEqual(type(result.statusbar.actionFoo), QtGui.QAction)
+
+    def testCustomWidgets(self):
+        w = QtGui.QWidget()
+        loader = QUiLoader()
+
+        filePath = os.path.join(os.path.dirname(__file__), 'customwidget.ui')
+        result = loader.load(filePath, w)
+        self.assert_(type(result.declarativeView), QtDeclarative.QDeclarativeView)
+        self.assert_(type(result.worldTimeClock), QtGui.QWidget)
+
 
 if __name__ == '__main__':
     unittest.main()
