673c0d26757526a068e758195d235c047150d789
Hugo Parente Lima <hugo.pl@gmail.com>
2010-10-06 11:40:42 -0300

    Added documentation to QPyTextObject class.

---
+++ PySide/qpytextobject.cpp
@@ -0,0 +1,33 @@
+/*
+ * This file is part of the PySide project.
+ *
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Contact: PySide team <contact@pyside.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include "qpytextobject.h"
+
+/*!
+    \class QPyTextObject
+    \brief Workaround to make possible use QTextObjectInterface on PySide.
+    \ingroup richtext-processing
+    Due to the technical details of how to bind C++ classes to Python, you need to use this class when you need to implement
+    your own QTextObjectInterface rather than create a class inheriting from QObject and QTextObjectInterface.
+
+    \sa QTextObjectInterface
+*/
