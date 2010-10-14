5c9afb798ecc9ec29622464c9c96909a2e79f220
renatofilho <renato.filho@openbossa.org>
2010-10-14 16:32:29 -0300

    WId conversions used on Windows OS.
    Fixes bug #399.
    
    Reviewer: Hugo Parente Lima <hugo.pl@gmail.com>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ PySide/QtGui/glue/wid_conversions.h
@@ -0,0 +1,32 @@
+namespace Shiboken {
+template <>
+struct Converter<WId>
+{
+    static bool checkType(PyObject* pyObj)
+    {
+        return false;
+    }
+
+    static bool isConvertible(PyObject* pyobj)
+    {
+        return PyCObject_Check(pyobj);
+    }
+
+    static inline PyObject* toPython(void* cppobj)
+    {
+        // not supported
+        Q_ASSERT(true);
+        return 0;
+    }
+
+    static PyObject* toPython(WId cppobj)
+    {
+        return PyCObject_FromVoidPtr(cppobj, 0);
+    }
+
+    static WId toCpp(PyObject* pyobj)
+    {
+        return (WId) PyCObject_AsVoidPtr(pyobj);
+    }
+};
+}
