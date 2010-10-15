d75bd8367d26e4049b6748cc001ec7482fbda832
renatofilho <renato.filho@openbossa.org>
2010-10-15 17:04:30 -0300

    Created function value to class QTreeWidgetItemIterator using the operator *.
    Fixes bug #400.
    
    Reviewer: Hugo Parente Lima <hugo.pl@gmail.com>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/QtGui/bug_400.py
@@ -0,0 +1,26 @@
+''' Test bug 389: http://bugs.openbossa.org/show_bug.cgi?id=389'''
+
+import unittest
+from helper import UsesQApplication
+from PySide.QtGui import QTreeWidgetItemIterator, QTreeWidgetItem, QTreeWidget
+
+class BugTest(UsesQApplication):
+    def testCase(self):
+        treeWidget = QTreeWidget()
+        treeWidget.setColumnCount(1)
+        items = []
+        for i in range(10):
+            items.append(QTreeWidgetItem(None, ["item: %i" % i]))
+
+        treeWidget.insertTopLevelItems(0, items);
+        _iter = QTreeWidgetItemIterator(treeWidget)
+        index = 0
+        while(_iter.value()):
+            item = _iter.value()
+            self.assert_(item is items[index])
+            index += 1
+            _iter += 1
+
+
+if __name__ == '__main__':
+    unittest.main()
