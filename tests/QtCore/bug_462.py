dd8b0fcfe41222be0141e2b717bb6f6b4c6f7107
Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
2010-11-14 21:02:34 -0300

    Created unit test for bug 462.
    Reviewer: Hugo Parente Lima <hugo.pl@gmail.com>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/QtCore/bug_462.py
@@ -0,0 +1,43 @@
+import unittest
+import sys
+
+from PySide.QtCore import QObject, QCoreApplication, QEvent, QThread
+
+class MyEvent(QEvent):
+    def __init__(self,i):
+        super(MyEvent,self).__init__(QEvent.Type(QEvent.User + 100 ))
+        self.i = i
+
+class MyThread (QThread):
+    def __init__(self,owner):
+        super(MyThread,self).__init__()
+        self.owner=owner;
+
+    def run(self):
+        for i in xrange(3):
+            e=MyEvent(i);
+            QCoreApplication.postEvent(self.owner,e)
+
+class MyBaseObject(QObject):
+    def __init__(self):
+        QObject.__init__(self)
+        self.events = []
+        self.t = MyThread(self)
+        self.t.start()
+
+    def customEvent(self, event):
+        self.events.append(event)
+        if len(self.events) == 3:
+            self.app.quit()
+
+
+class CheckForEventsTypes(unittest.TestCase):
+    def testTypes(self):
+        o = MyBaseObject()
+        o.app = QCoreApplication(sys.argv)
+        o.app.exec_()
+        for e in o.events:
+            self.assert_(isinstance(e, MyEvent))
+
+if __name__ == '__main__':
+    unittest.main()
