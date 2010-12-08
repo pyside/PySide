b548a6f1736a39061f51eb1020bc4f6e5259279a
Hugo Parente Lima <hugo.pl@gmail.com>
2010-12-08 14:54:21 -0200

    Fix copyright year.

--- PySide/QtDeclarative/pysideqmlregistertype.h
+++ PySide/QtDeclarative/pysideqmlregistertype.h
@@ -1,7 +1,7 @@
 /*
  * This file is part of the Shiboken Python Bindings Generator project.
  *
- * Copyright (C) 2009-2010 Nokia Corporation and/or its subsidiary(-ies).
+ * Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
  *
  * Contact: PySide team <contact@pyside.org>
  *
