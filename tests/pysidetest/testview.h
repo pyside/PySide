86b7d6e25c2612313da8a9193c90cc60728d3264
Marcelo Lira <marcelo.lira@openbossa.org>
2010-12-13 17:47:16 -0300

    Added test for Python written models returning new objects without keeping reference to them.
    Reviewed by Lauro Moura <lauro.neto@openbossa.org>
    eviewed by Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/pysidetest/testview.h
@@ -0,0 +1,23 @@
+#ifndef TESTVIEW_H
+#define TESTVIEW_H
+
+#include <QObject>
+#include <QAbstractListModel>
+#ifdef pysidetest_EXPORTS
+#define PYSIDE_EXPORTS 1
+#endif
+#include "pysidemacros.h"
+
+class PYSIDE_API TestView : public QObject
+{
+    Q_OBJECT
+public:
+    TestView(QAbstractListModel* model, QObject* parent = 0) : QObject(parent), m_model(model) {}
+    QAbstractListModel* model() { return m_model; }
+    QObject* getData();
+private:
+    QAbstractListModel* m_model;
+};
+
+#endif // TESTVIEW_H
+
