97875193524f407ec4594969041d84f7aa8ec546
Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
2010-12-27 18:59:32 -0300

    Fix QTreeWidget parent rules.
    QTreeWidget.clear() - remove all child ref from the current widget
    QTreeWidgetItem.parent() - use default policy for returned value
    
    Fix bug #547
    
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

---
+++ tests/QtGui/bug_547.py
@@ -0,0 +1,39 @@
+""" Unittest for bug #547 """
+""" http://bugs.openbossa.org/show_bug.cgi?id=547 """
+
+from PySide import QtGui
+import sys
+import unittest
+
+class MyMainWindow(unittest.TestCase):
+    def testClearFunction(self):
+        app = QtGui.QApplication(sys.argv)
+        self._tree = QtGui.QTreeWidget()
+        self._tree.setColumnCount(2)
+        self._i1 = None
+        self._i11 = None
+
+        self._updateTree()
+        self.assertEqual(sys.getrefcount(self._i1), 3)
+        self.assertEqual(sys.getrefcount(self._i11), 3)
+
+        self._i11.parent().setExpanded(True)
+        self._i11.setExpanded(True)
+
+        self._updateTree()
+        self.assertEqual(sys.getrefcount(self._i1), 3)
+        self.assertEqual(sys.getrefcount(self._i11), 3)
+
+
+    def _updateTree(self):
+        self._tree.clear()
+        if self._i1 and self._i11:
+            self.assertEqual(sys.getrefcount(self._i1), 2)
+            self.assertEqual(sys.getrefcount(self._i11), 2)
+
+        self._i1 = QtGui.QTreeWidgetItem(self._tree, ['1', ])
+        self._i11 = QtGui.QTreeWidgetItem(self._i1, ['11', ])
+
+if __name__ == '__main__':
+    unittest.main()
+
