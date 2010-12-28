d8f3b9629a6b1f1ab91516fdc144eb4fd80e5128
Hugo Parente Lima <hugo.pl@gmail.com>
2010-12-28 18:53:06 -0200

    Fix bug#569 - "QTableWidgetItem is missing binding of __lt__ to operator<"
    Reviewer: Renato Araújo <renato.filho@openbossa.org>
              Marcelo Lira <marcelo.lira@openbossa.org>

---
+++ tests/QtGui/bug_569.py
@@ -0,0 +1,19 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+import unittest
+
+
+class TestBug569(unittest.TestCase):
+
+    def testIt(self):
+        types = (QTableWidgetItem, QListWidgetItem, QTreeWidgetItem)
+        for t in types:
+            a = t()
+            a.__lt__ = lambda(other) : True
+            b = t()
+            b.__lt__ = lambda(other) : False
+            self.assertTrue(a < b)
+            self.assertFalse(b < a)
+
+if __name__ == '__main__':
+    unittest.main()
