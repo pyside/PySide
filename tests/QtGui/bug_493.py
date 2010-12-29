249a87808ac42644fecf9b8787b0ca6c757a2f4b
Hugo Parente Lima <hugo.pl@gmail.com>
2010-12-29 18:57:58 -0200

    Fix bug#493 - "__eq__ and friends not implemented for QKeyEvent == QKeySequence"
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Lauro Moura <lauro.neto@openbossa.org>

---
+++ tests/QtGui/bug_493.py
@@ -0,0 +1,21 @@
+from PySide.QtCore import *
+from PySide.QtGui import *
+import unittest
+
+
+class TestBug569(unittest.TestCase):
+
+    def testIt(self):
+        # We need a qapp otherwise Qt will crash when trying to detect the
+        # current platform
+        app = QApplication([])
+        ev1 = QKeyEvent(QEvent.KeyRelease, Qt.Key_Delete, Qt.NoModifier)
+        ev2 = QKeyEvent(QEvent.KeyRelease, Qt.Key_Copy, Qt.NoModifier)
+        ks = QKeySequence.Delete
+        self.assertEqual(ev1, ks)
+        self.assertEqual(ks, ev1)
+        self.assertNotEqual(ev2, ks)
+        self.assertNotEqual(ks, ev2)
+
+if __name__ == '__main__':
+    unittest.main()
