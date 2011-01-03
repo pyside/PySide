f9587fcad277a6d55afe1aee04759a273a632fe9
Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
2011-01-03 13:43:29 -0300

    Fixed QWidget.parent function.
    Create unit test for bug 576.
    Fixes bug #576.
    
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Lauro Moura <lauro.neto@openbossa.org>

---
+++ tests/QtGui/bug_576.py
@@ -0,0 +1,32 @@
+""" Unittest for bug #576 """
+""" http://bugs.openbossa.org/show_bug.cgi?id=576 """
+
+from PySide import QtGui, QtCore
+import sys
+import unittest
+
+class Bug576(unittest.TestCase):
+    def onButtonDestroyed(self, button):
+        self._destroyed = True
+
+    def testWidgetParent(self):
+        self._destroyed = False
+        app = QtGui.QApplication(sys.argv)
+        w = QtGui.QWidget()
+
+        b = QtGui.QPushButton("test")
+        b.destroyed[QtCore.QObject].connect(self.onButtonDestroyed)
+        self.assertEqual(sys.getrefcount(b), 2)
+        b.setParent(w)
+        self.assertEqual(sys.getrefcount(b), 3)
+        b.parent()
+        self.assertEqual(sys.getrefcount(b), 3)
+        b.setParent(None)
+        self.assertEqual(sys.getrefcount(b), 2)
+        del b
+        self.assert_(self._destroyed)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
