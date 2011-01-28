3c3caf154eadeb9b4bfda172f5441d023bc42044
Renato Araujo Oliveira Filho <renato.filho@openbossa.org>
2011-01-28 15:58:21 -0300

    Created unit test for bug #617.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Marcelo Lira <marcelo.lira@openbossa.org>

---
+++ tests/QtGui/bug_617.py
@@ -0,0 +1,21 @@
+from PySide.QtCore import QEvent
+from PySide.QtGui import QColor
+import unittest
+
+class MyEvent(QEvent):
+    def __init__(self):
+        QEvent.__init__(self, QEvent.Type(999))
+
+
+class Bug617(unittest.TestCase):
+    def testRepr(self):
+        c = QColor.fromRgb(1, 2, 3, 4)
+        s = c.spec()
+        self.assertEqual(repr(s), repr(QColor.Rgb))
+
+    def testOutOfBounds(self):
+        e = MyEvent()
+        self.assertEqual(repr(e.type()), '<enum-item Type.#out of bounds# (999)>')
+
+if __name__ == "__main__":
+   unittest.main()
