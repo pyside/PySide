d3fb0b38b76eb9a08a60c1a5667388afe9521fea
Renato Filho <renato.filho@openbossa.org>
2011-03-11 15:57:27 -0300

    Created unit test for bug #711.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Lauro Moura <lauro.neto@openbossa.org>

---
+++ tests/QtGui/bug_711.py
@@ -0,0 +1,21 @@
+import unittest
+import sys
+from PySide.QtGui import QToolBar, QApplication, QAction, QToolButton
+
+class TestLabelPixmap(unittest.TestCase):
+    def testReference(self):
+        toolbar = QToolBar()
+
+        for i in xrange(20):
+            toolbar.addAction(QAction("Action %d" % i, None))
+
+	buttons = toolbar.findChildren(QToolButton, "")
+        toolbar.clear()
+
+        for b in buttons:
+            self.assertRaises(RuntimeError, b.objectName)
+
+if __name__ == '__main__':
+    app = QApplication([])
+    unittest.main()
+
