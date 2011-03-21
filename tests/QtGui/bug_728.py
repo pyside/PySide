189717cdb196e4f2f2d47f6fa93b78d2e2e47a42
Hugo Parente Lima <hugo.pl@gmail.com>
2011-03-21 15:11:03 -0300

    Fix bug 728 - "QFileDialog.getOpenFileNames never returns (hangs)"
    Reviewer: Lauro Moura <lauro.neto@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/QtGui/bug_728.py
@@ -0,0 +1,8 @@
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+app = QApplication([])
+QTimer.singleShot(200, app.quit)
+
+# This test for a dead lock in QFileDialog.getOpenFileNames, the test fail with a timeout if the dead lock exists.
+QFileDialog.getOpenFileNames(None, "caption", QDir.homePath(), None, QFileDialog.DontUseNativeDialog)
