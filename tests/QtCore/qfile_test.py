6ef7460a47674072b2dd2f8521bea8c84f7153d5
Hugo Parente Lima <hugo.pl@gmail.com>
2011-04-06 18:35:49 -0300

    Fix bug 721 - "QFile doesn't support map and unmap functions."

--- tests/QtCore/qfile_test.py
+++ tests/QtCore/qfile_test.py
@@ -27,5 +27,16 @@ class GetCharTest(unittest.TestCase):
         self.assert_(not obj.getChar()[0])
         obj.close()
 
+    def testBug721(self):
+        obj = QFile(self.filename)
+        obj.open(QIODevice.ReadOnly)
+        memory = obj.map(0, 1)
+        self.assertEqual(len(memory), 1)
+        self.assertEqual(memory[0], 'a')
+        obj.unmap(memory)
+        # now memory points to wild bytes... :-)
+        # uncommenting this must cause a segfault.
+        # self.assertEqual(memory[0], 'a')
+
 if __name__ == '__main__':
     unittest.main()
