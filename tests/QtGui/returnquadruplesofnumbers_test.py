39c32e371237580fbfb746e068351fe0e83636b6
Marcelo Lira <marcelo.lira@openbossa.org>
2011-04-13 18:26:00 -0300

    Fixes bug #742 - Method "void QPrinter::getPageMargins(qreal*,qreal*,qreal*,qreal*,Unit) const" missing
    Also added unit tests.
    
    http://bugs.pyside.org/show_bug.cgi?id=742

--- tests/QtGui/returnquadruplesofnumbers_test.py
+++ tests/QtGui/returnquadruplesofnumbers_test.py
@@ -1,5 +1,5 @@
 import unittest
-from PySide.QtGui import QLayout, QWidget, QGraphicsLayout, QGraphicsLayoutItem, QTextCursor
+from PySide.QtGui import QLayout, QWidget, QGraphicsLayout, QGraphicsLayoutItem, QTextCursor, QPrinter
 
 from helper import UsesQApplication
 
@@ -42,6 +42,13 @@ class ReturnsQuadruplesOfNumbers(UsesQApplication):
     def testQTextCursorSelectedTableCells(self):
         obj = QTextCursor()
         self.assertEquals(obj.selectedTableCells(), (-1, -1, -1, -1))
+
+    def testQPrinterGetPageMargins(self):
+        # Bug #742
+        obj = QPrinter()
+        values = (10.0, 20.0, 30.0, 40.0, QPrinter.Point)
+        obj.setPageMargins(*values)
+        self.assertEquals(obj.getPageMargins(QPrinter.Point), values[:-1])
 
 if __name__ == "__main__":
    unittest.main()
