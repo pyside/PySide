2a788ba61801c858a739b0e7947dc5a6589e686d
Renato Filho <renato.filho@openbossa.org>
2011-04-13 15:10:50 -0300

    Created unit test for bug #722.
    Reviewer: Luciano Wolf <luciano.wolf@openbossa.org>
              Lauro Moura <lauro.neto@openbossa.org>

---
+++ tests/QtGui/bug_722.py
@@ -0,0 +1,24 @@
+import unittest
+
+from helper import UsesQApplication
+
+from PySide.QtGui import QDoubleSpinBox, QGraphicsBlurEffect
+
+class TestSignalConnection(UsesQApplication):
+    def testFloatSignal(self):
+        foo1 = QDoubleSpinBox()
+        foo2 = QDoubleSpinBox()
+        foo1.valueChanged[float].connect(foo2.setValue)
+        foo2.valueChanged[float].connect(foo1.setValue)
+        foo1.setValue(0.42)
+        self.assertEqual(foo1.value(), foo2.value())
+    
+    def testQRealSignal(self):
+        foo1 = QDoubleSpinBox()
+        effect = QGraphicsBlurEffect()
+        effect.blurRadiusChanged['qreal'].connect(foo1.setValue) # check if qreal is a valid type
+        effect.setBlurRadius(0.42)
+        self.assertEqual(foo1.value(), effect.blurRadius())
+
+if __name__ == '__main__':
+    unittest.main()
