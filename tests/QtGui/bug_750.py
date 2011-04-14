adf3d712d5c41eec81a69429c12df2c1851b9020
Renato Filho <renato.filho@openbossa.org>
2011-04-14 09:53:12 -0300

    Created unittest for bug #750.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

---
+++ tests/QtGui/bug_750.py
@@ -0,0 +1,24 @@
+import unittest
+
+from helper import UsesQApplication
+
+from PySide.QtGui import QPainter, QFont, QFontInfo, QWidget, qApp
+
+class MyWidget(QWidget):
+    def paintEvent(self, e):
+        p = QPainter(self)
+        self._info = p.fontInfo()
+        self._app.quit()
+
+
+class TestQPainter(UsesQApplication):
+    def testFontInfo(self):
+        w = MyWidget()
+        w._app = self.app
+        w._info = None
+        w.show()
+        self.app.exec_()
+        self.assert_(w._info)
+
+if __name__ == '__main__':
+    unittest.main()
