cd1dbb0b9da900e83cb1ce62e20e901e1b24cd6d
Renato Filho <renato.filho@openbossa.org>
2011-04-20 18:43:51 -0300

    Created unit test for function QPicture.setData
    Reviewer: Lauro Moura <lauro.neto@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

---
+++ tests/QtGui/qpicture_test.py
@@ -0,0 +1,35 @@
+import unittest
+
+import os
+from helper import UsesQApplication
+from PySide.QtGui import QPicture, QPainter, QWidget
+
+class MyWidget(QWidget):
+    def paintEvent(self, e):
+        p = QPainter(self)
+        p.drawPicture(0, 0, self._picture)
+        self._app.quit()
+
+class QPictureTest(UsesQApplication):
+    def testFromData(self):
+        picture = QPicture()
+        painter = QPainter()
+        painter.begin(picture)
+        painter.drawEllipse(10,20, 80,70)
+        painter.end()
+
+        data = picture.data()
+        picture2 = QPicture()
+        picture2.setData(data)
+
+        self.assertEqual(picture2.data(), picture.data())
+
+        w = MyWidget()
+        w._picture = picture2
+        w._app = self.app
+        w.show()
+        self.app.exec_()
+
+if __name__ == '__main__':
+    unittest.main()
+
