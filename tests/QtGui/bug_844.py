9b793705feae3da73230198d61cb29627b558e6a
Hugo Parente Lima <hugo.pl@gmail.com>
2011-05-03 15:03:44 -0300

    Fix bug 844 - "Crash in QGraphicsItem::toGraphicsObject when printing obj reference"
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Renato Araújo <renato.filho@openbossa.org>

---
+++ tests/QtGui/bug_844.py
@@ -0,0 +1,19 @@
+from PySide.QtGui import *
+from PySide.QtCore import *
+
+class QtKeyPressListener(QObject):
+    def __init__(self, obj):
+        QObject.__init__(self)
+        obj.installEventFilter(self)
+        self.fConnections = {}
+
+    def eventFilter(self, obj, event):
+        # This used to crash here due to a misbehaviour of type discovery!
+        return QObject.eventFilter(self, obj, event)
+
+app = QApplication([])
+key_listener = QtKeyPressListener(app)
+w = QLabel('Hello')
+w.show()
+QTimer.singleShot(0, w.close)
+app.exec_()
