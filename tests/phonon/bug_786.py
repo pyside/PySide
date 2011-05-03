06051ca647627e4c158ae5745867093926d512a8
Hugo Parente Lima <hugo.pl@gmail.com>
2011-05-03 19:10:01 -0300

    Fix bug 786 -  "There's no __eq__ for all classes inherited from ObjectDescription<T> due to an Apiextractor bug."
    Reviewer: Lauro Moura <lauro.neto@openbossa.org>
              Renato Araújo <renato.filho@openbossa.org>

---
+++ tests/phonon/bug_786.py
@@ -0,0 +1,21 @@
+import unittest
+from PySide.QtGui import *
+from PySide.phonon import *
+
+
+class TestBug786 (unittest.TestCase):
+
+    def testIt(self):
+        app = QApplication([])
+        devices = Phonon.BackendCapabilities.availableAudioOutputDevices()
+        if len(devices) > 1:
+            self.assertNotEqual(devices[0], devices[1])
+            self.assertEqual(devices[0], devices[0])
+
+        effects = Phonon.BackendCapabilities.availableAudioEffects()
+        if len(effects) > 1:
+            self.assertNotEqual(effects[0], effects[1])
+            self.assertEqual(effects[0], effects[0])
+
+if __name__ == '__main__':
+    unittest.main()
