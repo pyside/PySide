fc331f37e3ee96a70123946e14ab29e7bc3bf8a3
Renato Filho <renato.filho@openbossa.org>
2011-05-09 19:50:10 -0300

    Created test for mew pythonic functions on QTransform class.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

--- tests/QtGui/qtransform_test.py
+++ tests/QtGui/qtransform_test.py
@@ -1,5 +1,6 @@
 import unittest
-from PySide.QtGui import QTransform
+from PySide.QtCore import QPointF
+from PySide.QtGui import QTransform, QPolygonF, QPolygonF
 
 class QTransformTest(unittest.TestCase):
 
@@ -10,6 +11,62 @@ class QTransformTest(unittest.TestCase):
         self.assert_(isinstance(tx, float))
         self.assert_(isinstance(ty, float))
         self.assertEqual((tx, ty), values)
+
+    def testquadToQuad(self):
+        q1 = QPolygonF()
+        q1.append(QPointF(10.0, 10.0))
+        q1.append(QPointF(20.0, 10.0))
+        q1.append(QPointF(10.0, -10.0))
+        q1.append(QPointF(20.0, -10.0))
+
+        q2 = QPolygonF()
+        q2.append(QPointF(20.0, 20.0))
+        q2.append(QPointF(30.0, 20.0))
+        q2.append(QPointF(20.0, -20.0))
+        q2.append(QPointF(30.0, -20.0))
+
+        t1 = QTransform()
+        r1 = QTransform.quadToQuad(q1, q2, t1)
+        r2 = QTransform.quadToQuad(q1, q2)
+
+        self.assertTrue(r1)
+        self.assert_(r2)
+
+        self.assertEqual(t1, r2)
+
+    def testquadToSquare(self):
+        q1 = QPolygonF()
+        q1.append(QPointF(10.0, 10.0))
+        q1.append(QPointF(20.0, 10.0))
+        q1.append(QPointF(10.0, -10.0))
+        q1.append(QPointF(20.0, -10.0))
+
+        t1 = QTransform()
+        r1 = QTransform.quadToSquare(q1, t1)
+        r2 = QTransform.quadToSquare(q1)
+
+        self.assertTrue(r1)
+        self.assert_(r2)
+
+        self.assertEqual(t1, r2)
+
+
+    def testsquareToQuad(self):
+        q1 = QPolygonF()
+        q1.append(QPointF(10.0, 10.0))
+        q1.append(QPointF(20.0, 10.0))
+        q1.append(QPointF(10.0, -10.0))
+        q1.append(QPointF(20.0, -10.0))
+
+        t1 = QTransform()
+        r1 = QTransform.squareToQuad(q1, t1)
+        r2 = QTransform.squareToQuad(q1)
+
+        self.assertTrue(r1)
+        self.assert_(r2)
+
+        self.assertEqual(t1, r2)
+
 
 if __name__ == "__main__":
    unittest.main()
