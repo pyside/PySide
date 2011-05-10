a5249a70e13891884ed62bc47558897377a4b800
Hugo Parente Lima <hugo.pl@gmail.com>
2011-05-10 18:11:55 -0300

    Use add-function on QCoreApplication and QApplication constructors.
    This was needed after a fix in the shiboken generator regarding the
    tp_init initialization.

--- PySide/QtCore/glue/qcoreapplication_init.cpp
+++ PySide/QtCore/glue/qcoreapplication_init.cpp
@@ -2,43 +2,23 @@
 static int QCoreApplicationArgCount;
 static char** QCoreApplicationArgValues;
 
-int Sbk_QCoreApplication_Init(PyObject* self, PyObject* args, PyObject*)
+void QCoreApplication_constructor(PyObject* self, PyObject* args, QCoreApplicationWrapper** cptr)
 {
-    if (Shiboken::Object::isUserType(self) && !Shiboken::ObjectType::canCallConstructor(self->ob_type, Shiboken::SbkType<QCoreApplication >()))
-        return -1;
-
-
     if (QCoreApplication::instance()) {
         PyErr_SetString(PyExc_RuntimeError, "A QCoreApplication instance already exists.");
-        return -1;
+        return;
     }
 
     int numArgs = PyTuple_GET_SIZE(args);
-    if (numArgs != 1) {
+    if (numArgs != 1
+        || !Shiboken::sequenceToArgcArgv(PyTuple_GET_ITEM(args, 0), &QCoreApplicationArgCount, &QCoreApplicationArgValues, "PySideApp")) {
         PyErr_BadArgument();
-        return -1;
+        return;
     }
 
-    if (!Shiboken::sequenceToArgcArgv(PyTuple_GET_ITEM(args, 0), &QCoreApplicationArgCount, &QCoreApplicationArgValues, "PySideApp")) {
-        PyErr_BadArgument();
-        return -1;
-    }
+    *cptr = new QCoreApplicationWrapper(QCoreApplicationArgCount, QCoreApplicationArgValues);
 
-    SbkObject* sbkSelf = reinterpret_cast<SbkObject*>(self);
-    QCoreApplicationWrapper* cptr = new QCoreApplicationWrapper(QCoreApplicationArgCount, QCoreApplicationArgValues);
-    Shiboken::Object::setCppPointer(sbkSelf,
-                                     Shiboken::SbkType<QCoreApplication>(),
-                                     cptr);
-
-    Shiboken::Object::setValidCpp(sbkSelf, true);
-    Shiboken::Object::setHasCppWrapper(sbkSelf, true);
-    Shiboken::Object::releaseOwnership(sbkSelf);
-
-    Shiboken::BindingManager::instance().registerWrapper(sbkSelf, cptr);
-    PySide::Signal::updateSourceObject(self);
-    cptr->metaObject();
-
+    Shiboken::Object::releaseOwnership(reinterpret_cast<SbkObject*>(self));
     PySide::registerCleanupFunction(&PySide::destroyQCoreApplication);
     Py_INCREF(self);
-    return 1;
 }
