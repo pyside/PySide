7109418dbeac85be5513fa2a6e974576219e7582
Renato Filho <renato.filho@openbossa.org>
2011-05-31 15:45:44 -0300

    Created unit test for QObject.eventFilter function.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Luciano Wolf <luciano.wolf@openbossa.org>

---
+++ tests/QtGui/event_filter_test.py
@@ -0,0 +1,39 @@
+import unittest
+import sys
+
+from helper import UsesQApplication
+from PySide.QtCore import QObject, QEvent, QTimer
+from PySide.QtGui import QWidget
+
+class MyFilter(QObject):
+  def eventFilter(self, obj, event):
+    if event.type() == QEvent.KeyPress:
+      pass
+    return QObject.eventFilter(self, obj, event)
+
+
+class EventFilter(UsesQApplication):
+    def testRefCount(self):
+        o = QObject()
+        filt = MyFilter()
+        o.installEventFilter(filt)
+        self.assertEqual(sys.getrefcount(o), 3)
+
+        o.installEventFilter(filt)
+        self.assertEqual(sys.getrefcount(o), 3)
+
+        o.removeEventFilter(filt)
+        self.assertEqual(sys.getrefcount(o), 2)
+
+    def testObjectDestructorOrder(self):
+        w = QWidget()
+        filt = MyFilter()
+        filt.app = self.app
+        w.installEventFilter(filt)
+        w.show()
+        w.close()
+        w = None
+        self.assert_(True)
+
+if __name__ == '__main__':
+    unittest.main()
