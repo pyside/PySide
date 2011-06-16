db40ec67b10e50310dea7d8b9a1bb5c734c232ba
Renato Filho <renato.filho@openbossa.org>
2011-06-16 17:00:27 -0300

    Fill qApp is any QApplication is active during the QtGui moude import.
    Reviewer: Marcelo Lira <marcelo.lira@openbossa.org>
              Hugo Parente Lima <hugo.pl@gmail.com>

--- PySide/QtGui/glue/qtgui_qapp.cpp
+++ PySide/QtGui/glue/qtgui_qapp.cpp
@@ -1,4 +1,10 @@
 // Init qApp macro to None.
-Py_INCREF(Py_None);
-PyModule_AddObject(module, "qApp", Py_None);
+if (qApp) {
+    PyObject* pyApp = Shiboken::Converter<QApplication*>::toPython(qApp);
+    Py_INCREF(pyApp);
+    PyModule_AddObject(module, "qApp", pyApp);
+} else {
+    Py_INCREF(Py_None);
+    PyModule_AddObject(module, "qApp", Py_None);
+}
 moduleQtGui = module;
