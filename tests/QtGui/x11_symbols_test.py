08d202e824ffc6221bd426084ac169324808e9da
Hugo Parente Lima <hugo.pl@gmail.com>
2011-08-31 15:24:40 -0300

    Fix bug 966 - "QX11Info.display() missing"

--- tests/QtGui/x11_symbols_test.py
+++ tests/QtGui/x11_symbols_test.py
@@ -3,15 +3,22 @@
 
 import unittest
 
-from PySide.QtGui import QPixmap
+from PySide.QtGui import *
 
 class X11Test(unittest.TestCase):
 
     def test(self):
-        self.assert_('handle' in dir(QPixmap))
-        self.assert_('x11Info' in dir(QPixmap))
-        self.assert_('x11PictureHandle' in dir(QPixmap))
-        self.assert_('x11SetDefaultScreen' in dir(QPixmap))
+        qpixmapFuncs = dir(QPixmap)
+        self.assert_('handle' in qpixmapFuncs)
+        self.assert_('x11Info' in qpixmapFuncs)
+        self.assert_('x11PictureHandle' in qpixmapFuncs)
+        self.assert_('x11SetDefaultScreen' in qpixmapFuncs)
+
+    def testX11Functions(self):
+        qx11infoFuncs = dir(QX11Info)
+        self.assert_('display' in qx11infoFuncs)
+        self.assert_('appVisual' in qx11infoFuncs)
+        self.assert_('visual' in qx11infoFuncs)
 
 if __name__ == '__main__':
     unittest.main()
