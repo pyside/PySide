60f657f0174e8e95e1d30a605dd87d70d64f73cd
Hugo Parente Lima <hugo.pl@gmail.com>
2011-09-13 17:44:17 -0300

    Unit test for bug 995 - "QDeclarativeView.itemAt returns faulty reference. (leading to SEGFAULT)"

---
+++ tests/QtDeclarative/bug_995.py
@@ -0,0 +1,13 @@
+from PySide.QtGui import *
+from PySide.QtDeclarative import *
+from helper import adjust_filename, UsesQApplication
+
+app = QApplication([])
+view = QDeclarativeView(adjust_filename('bug_995.qml', __file__))
+view.show()
+view.resize(200, 200)
+item = view.itemAt(100, 100)
+
+# it CAN NOT crash here
+print item
+
---
+++ tests/QtDeclarative/bug_995.qml
@@ -0,0 +1,12 @@
+import Qt 4.7
+
+Rectangle {
+    width: 100
+    height: 100
+    color: "red"
+
+    Text {
+        text: "Hello World"
+        anchors.centerIn: parent
+    }
+}
