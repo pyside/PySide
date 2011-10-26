65b1748453c5d6bd325ac42b3d09b9aab6d1d01b
Hugo Parente Lima <hugo.pl@gmail.com>
2011-10-26 14:37:28 -0200

    Fix code style and add license headers.

--- tests/pysidetest/testobject.cpp
+++ tests/pysidetest/testobject.cpp
@@ -1,32 +1,48 @@
+/*
+ * This file is part of the PySide project.
+ *
+ * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+ *
+ * Contact: PySide team <contact@pyside.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 #include "testobject.h"
 
-void
-TestObject::emitIdValueSignal()
+void TestObject::emitIdValueSignal()
 {
     emit idValue(m_idValue);
 }
 
-void
-TestObject::emitStaticMethodDoubleSignal()
+void TestObject::emitStaticMethodDoubleSignal()
 {
     emit staticMethodDouble();
 }
 
-void
-TestObject::emitSignalWithDefaultValue_void()
+void TestObject::emitSignalWithDefaultValue_void()
 {
     emit signalWithDefaultValue();
 }
 
-void
-TestObject::emitSignalWithDefaultValue_bool()
+void TestObject::emitSignalWithDefaultValue_bool()
 {
     emit signalWithDefaultValue(true);
 }
 
-void
-TestObject::emitSignalWithTypedefValue(int value)
+void TestObject::emitSignalWithTypedefValue(int value)
 {
     emit signalWithTypedefValue(TypedefValue(value));
 }
-
