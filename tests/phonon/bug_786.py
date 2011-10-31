0c7399f821843326510b9cdb2c4d967fc2ea64a4
Hugo Parente Lima <hugo.pl@gmail.com>
2011-10-31 16:29:09 -0200

    Fix phonon unit test when there are device aliases reported by phonon.

--- tests/phonon/bug_786.py
+++ tests/phonon/bug_786.py
@@ -7,9 +7,11 @@ class TestBug786 (unittest.TestCase):
 
     def testIt(self):
         app = QApplication([])
+        app.setApplicationName("PySide Phonon Unit Test")
         devices = Phonon.BackendCapabilities.availableAudioOutputDevices()
         if len(devices) > 1:
-            self.assertNotEqual(devices[0], devices[1])
+            if devices[0].index() != devices[0].index():
+                self.assertNotEqual(devices[0], devices[1])
             self.assertEqual(devices[0], devices[0])
 
         effects = Phonon.BackendCapabilities.availableAudioEffects()
