7b04273736e46ace2d22b568fa7ed4d23944b067
Marcelo Lira <marcelo.lira@openbossa.org>
2011-11-30 17:47:35 -0300

    Fixed code for QUdpSocket.readDatagram method to remove usage of Shiboken::makeTuple.

--- PySide/QtNetwork/glue/qudpsocket_read_datagram_glue.cpp
+++
@@ -1,6 +0,0 @@
-    Shiboken::AutoArrayPointer<char> data(%ARGUMENT_NAMES);
-    QHostAddress ha;
-    quint16 port;
-
-    %RETURN_TYPE retval = %CPPSELF.%FUNCTION_NAME(data, %ARGUMENT_NAMES, &ha, &port);
-    %PYARG_0 = Shiboken::makeTuple(QByteArray(data, retval), ha, port);
