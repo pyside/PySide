93bb5d777e6db0ba8d2b216832bf4211476c581e
Hugo Parente Lima <hugo.pl@gmail.com>
2011-12-09 11:21:38 -0200

    Fix bug 1083 - "winId returns a PyCObject making it impossible to compare two winIds"

--- PySide/QtGui/glue/wid_conversions.h
+++ PySide/QtGui/glue/wid_conversions.h
@@ -11,14 +11,7 @@ struct Converter<WId>
 
     static bool isConvertible(PyObject* pyobj)
     {
-        if (pyobj == Py_None)
-            return true;
-
-#ifdef IS_PY3K
-        return PyCapsule_CheckExact(pyobj);
-#else
-        return PyCObject_Check(pyobj);
-#endif
+        return pyobj == Py_None || PyNumber_Check(pyobj);
     }
 
     static inline PyObject* toPython(void* cppobj)
@@ -30,26 +23,14 @@ struct Converter<WId>
 
     static PyObject* toPython(WId cppobj)
     {
-        if (!cppobj)
-            Py_RETURN_NONE;
-
-#ifdef IS_PY3K
-        return PyCapsule_New(cppobj, 0, 0);
-#else
-        return PyCObject_FromVoidPtr(cppobj, 0);
-#endif
+        return PyLong_FromVoidPtr((void*)cppobj);
     }
 
     static WId toCpp(PyObject* pyobj)
     {
         if (pyobj == Py_None)
             return 0;
-
-#ifdef IS_PY3K
-        return (WId)PyCapsule_GetPointer(pyobj, 0);
-#else
-        return (WId)PyCObject_AsVoidPtr(pyobj);
-#endif
+        return (WId)PyLong_AsVoidPtr(pyobj);
     }
 };
 
