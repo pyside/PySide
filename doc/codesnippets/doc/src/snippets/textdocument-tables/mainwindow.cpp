fa2e571b6c68aa9439a88fc7ab441b6e091268df
Teemu Kaukoranta <teemu.kaukoranta@digia.com>
2013-05-20 15:26:59 +0300

    Fixed licenses
    License fixed using replace-licenses.zsh from mkdist repo.
    Possible copyright and contact changes will come later.
    
    Change-Id: I18b977ac33f1b00e99fa7aae6c1a824769ce5f6e
    Reviewed-by: Janne Anttila <janne.anttila@digia.com>
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>

--- doc/codesnippets/doc/src/snippets/textdocument-tables/mainwindow.cpp
+++ doc/codesnippets/doc/src/snippets/textdocument-tables/mainwindow.cpp
@@ -6,35 +6,35 @@
 ## This file is part of the documentation of the Qt Toolkit.
 ##
 ## $QT_BEGIN_LICENSE:LGPL$
-## Commercial Usage
-## Licensees holding valid Qt Commercial licenses may use self file in
-## accordance with the Qt Commercial License Agreement provided with the
+## Commercial License Usage
+## Licensees holding valid commercial Qt licenses may use this file in
+## accordance with the commercial license agreement provided with the
 ## Software or, alternatively, in accordance with the terms contained in
-## a written agreement between you and Nokia.
+## a written agreement between you and Digia.  For licensing terms and
+## conditions see http://qt.digia.com/licensing.  For further information
+## use the contact form at http://qt.digia.com/contact-us.
 ##
 ## GNU Lesser General Public License Usage
-## Alternatively, self file may be used under the terms of the GNU Lesser
+## Alternatively, this file may be used under the terms of the GNU Lesser
 ## General Public License version 2.1 as published by the Free Software
 ## Foundation and appearing in the file LICENSE.LGPL included in the
-## packaging of self file.  Please review the following information to
+## packaging of this file.  Please review the following information to
 ## ensure the GNU Lesser General Public License version 2.1 requirements
 ## will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
 ##
-## In addition, as a special exception, Nokia gives you certain
-## additional rights. These rights are described in the Nokia Qt LGPL
-## Exception version 1.0, included in the file LGPL_EXCEPTION.txt in self
-## package.
+## In addition, as a special exception, Digia gives you certain additional
+## rights.  These rights are described in the Digia Qt LGPL Exception
+## version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
 ##
 ## GNU General Public License Usage
-## Alternatively, self file may be used under the terms of the GNU
+## Alternatively, this file may be used under the terms of the GNU
 ## General Public License version 3.0 as published by the Free Software
 ## Foundation and appearing in the file LICENSE.GPL included in the
-## packaging of self file.  Please review the following information to
+## packaging of this file.  Please review the following information to
 ## ensure the GNU General Public License version 3.0 requirements will be
 ## met: http://www.gnu.org/copyleft/gpl.html.
 ##
-## If you are unsure which license is appropriate for your use, please
-## contact the sales department at http://www.qtsoftware.com/contact.
+##
 ## $QT_END_LICENSE$
 ##
 ###########################################################################
